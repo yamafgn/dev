527e057147970e62e0eeeb83c59d06b454832fbb
YamaFGN <yamafgn@gmail.com>
2017-06-08 21:18:45 +0900

    [update]bbb
    bbbb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "hello, my world!aaaaaaa"
+    render html: "hello, my world!aaaaaaabbbbb"
   end
 
   def goodbye
