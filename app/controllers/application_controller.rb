8ac36e0fe7097e53ce8232372b037d7825d3b736
YamaFGN <yamafgn@gmail.com>
2017-06-08 21:38:06 +0900

    [update]for pull request
    pull request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "hello, my world!aaaaaaabbbbb"
+    render html: "hello, my world!aaaaaaabbbbb for push request."
   end
 
   def goodbye
