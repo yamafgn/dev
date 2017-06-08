30de42fa10fbbb591954ec3db7c7c313c0e40af7
YamaFGN <yamafgn@gmail.com>
2017-06-08 20:48:17 +0900

    [update]change view message.
    changed word to my world lol.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "hello, world!"
+    render html: "hello, my world!"
   end
 
   def goodbye
